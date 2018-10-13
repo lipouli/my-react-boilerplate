955ccfe995964b0b509cb6719873d64efae137c3
lipouli <li.lipou@gmail.com>
2018-10-13 10:39:26 +0200

    add css support for webpack

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const App = () => (
-  <div>
-    App Online !
-  </div>
-);
+import './app.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: 1,
+    };
+  }
+
+  getMenu() {
+    return 1;
+  }
+
+  render() {
+    return (
+      <div>
+        App online ! {this.getMenu()}
+      </div>
+    );
+  }
+}
 
 export default App;
